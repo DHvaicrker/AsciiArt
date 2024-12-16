9090f871b09d857f096556faf11d931c0ff5cb65
Dvirmat <dvir.mateless@mail.huji.ac.il>
2024-12-16 21:29:36 +0200

    Shell base.

--- src/ascii_art/Shell.java
+++ src/ascii_art/Shell.java
@@ -1,19 +1,34 @@
 package ascii_art;
 
+import java.util.List;
+import java.util.TreeSet;
+import java.util.Arrays;
+
 public class Shell {
 
-    public static final int IMAGE_PATH_INDEX = 0;
-    public static final int COMMAND_IDX = 0;
-    public static final String COMMAND_PREFIX = ">>> ";
-    public static final String SPACE_REGEX = "\\s+";
-    public static final String EXIT_PROGRAM_COMMAND = "exit";
-    public static final String SHOW_CHARS_COMMAND = "chars";
-    public static final String ADD_CHAR_COMMAND = "add";
-    public static final String REMOVE_CHAR_COMMAND = "remove";
-    public static final String CHANGE_RES_COMMAND = "res";
-    public static final String CHANGE_OUTPUT_COMMAND = "output";
-    public static final String ROUND_METHOD_COMMAND = "round";
-    public static final String RUN_ALGORITHM_COMMAND = "asciiArt";
+    // constants
+    private static final int IMAGE_PATH_INDEX = 0;
+    private static final int COMMAND_IDX = 0;
+    private static final String COMMAND_PREFIX = ">>> ";
+    private static final String SPACE_REGEX = "\\s+";
+
+    private static final String EXIT_PROGRAM_COMMAND = "exit";
+    private static final String SHOW_CHARS_COMMAND = "chars";
+    private static final String ADD_CHAR_COMMAND = "add";
+    private static final String REMOVE_CHAR_COMMAND = "remove";
+    private static final String CHANGE_RES_COMMAND = "res";
+    private static final String CHANGE_OUTPUT_COMMAND = "output";
+    private static final String ROUND_METHOD_COMMAND = "round";
+    private static final String RUN_ALGORITHM_COMMAND = "asciiArt";
+    private static final List<Character> DEFAULT_CHAR_SET = Arrays.asList('1',
+            '2', '3', '4', '5', '6', '7', '8', '9', '0');
+    private static final int DEFAULT_RES = 2;
+    private static final String ABS_ROUND_METHOD = "abs";
+
+    // attributes
+    private TreeSet<Character> charSet = new TreeSet<>(DEFAULT_CHAR_SET);
+    private int resultion = DEFAULT_RES;
+    private String round_method = ABS_ROUND_METHOD;
 
     public Shell() {
     }
